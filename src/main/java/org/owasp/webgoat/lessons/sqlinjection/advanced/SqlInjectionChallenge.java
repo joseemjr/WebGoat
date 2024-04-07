0f2e9fb4c006a906488e6520547243d0b684ace3
aalmeidar <a.almeida.2021@alumnos.urjc.es>
2024-04-06 22:02:32 -0400

    Blind SQLi fixed
    Fixed with prepared statements

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionChallenge.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionChallenge.java
@@ -63,13 +63,12 @@ public class SqlInjectionChallenge extends AssignmentEndpoint {
     if (attackResult == null) {
 
       try (Connection connection = dataSource.getConnection()) {
-        String checkUserQuery =
-            "select userid from sql_challenge_users where userid = '" + username_reg + "'";
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(checkUserQuery);
+        PreparedStatement statement = connection.prepareStatement("SELECT userid FROM sql_challenge_users where userid = ?");
+        statement.setString(1, username_reg);
+        ResultSet resultSet = statement.executeQuery();
 
         if (resultSet.next()) {
-          if (username_reg.contains("tom'")) {
+            if (username_reg.contains("tom'")) {
             attackResult = success(this).feedback("user.exists").build();
           } else {
             attackResult = failed(this).feedback("user.exists").feedbackArgs(username_reg).build();
